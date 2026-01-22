a6edb8ffdb510a360822fc91ea46455fb01f0711
Durjoy Chakraborty <durjoyckb@gmail.com>
2026-01-22 10:13:53 +0600

    2nd day

---
+++ Day 02/LogicalOp.py
@@ -0,0 +1,10 @@
+#Logical operator...
+a = 50
+b = 30
+print(not (a > b))
+print(not(False))
+print(not(True))
+
+val1 = True
+val2 = True
+print(val1 and val2)
---
+++ Day 02/type.py
@@ -0,0 +1,11 @@
+#type conversion
+a = 2
+b = 4.25
+
+print(a+b)
+
+#type casting
+c = float("3")
+d = 6.25
+
+print(c + d)
