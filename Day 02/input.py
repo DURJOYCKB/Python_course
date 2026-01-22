04fdbf80129a0b1883f8dbee1965ff8e4e2c0af2
Durjoy Chakraborty <durjoyckb@gmail.com>
2026-01-22 19:58:33 +0600

    Day 02 done

---
+++ Day 02/input.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+age = int(input("Enter age: "))
+marks = float(input("enter marks: "))
+
+print("Welcome ", name)
+print("your age ", age)
+print("you got ", marks)
