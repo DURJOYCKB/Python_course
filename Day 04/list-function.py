76c69e73c3aeb3c45080a90e7d33f7ad85f0401a
Durjoy Chakraborty <durjoyckb@gmail.com>
2026-01-26 09:17:00 +0600

    Day 04 start

---
+++ Day 04/list-function.py
@@ -0,0 +1,13 @@
+list = [2, 1, 3]
+list.append(4)
+
+list.sort()               #Ascending sort...
+
+list.sort(reverse=True)   #Descending sort...
+
+list1 = ["d", "b", "a"]
+list1.reverse()
+print(list1)
+
+list.insert(1, 5)
+print(list)
