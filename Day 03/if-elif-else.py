3dcd77f85120c707c1c7955693b9851066cef3c7
Durjoy Chakraborty <durjoyckb@gmail.com>
2026-01-26 08:26:15 +0600

    Day 03 done

---
+++ Day 03/if-elif-else.py
@@ -0,0 +1,8 @@
+light = "green"
+
+if(light == "red"):
+    print("Stop")
+elif(light == "green"):
+    print("Go")
+else:
+    print("Look")
---
+++ Day 03/list.py
@@ -0,0 +1,8 @@
+marks = [12,23,34,45,56]
+print(marks)
+
+student = ["Durjoy", 24, "chattogram"]       #list can store all 
+print(student)                                #Datatypes....
+
+student[0] = "Arjun"
+print(student)                           #List is mutable/changable...
