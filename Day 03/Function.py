4f6e319545cb4e5b1a688966220191e5714faba4
Durjoy Chakraborty <durjoyckb@gmail.com>
2026-01-26 07:42:24 +0600

    Day 03 start

---
+++ Day 03/Function.py
@@ -0,0 +1,10 @@
+str = "i am a CSE student"
+print(str.endswith("ent"))   #True
+
+print(str.capitalize())      #i -> I...
+
+print(str.replace("a", "e"))      #replace character...
+
+print(str.find("C"))
+
+print(str.count("a"))
---
+++ Day 03/Slicing.py
@@ -0,0 +1,11 @@
+str = "Durjoy Chakraborty"
+
+print(str[3 : 6])                         #joy
+print(str[7:len(str)])                    #Chakraborty
+      
+print(str[:6])    #[0:6]                  #Durjoy
+print(str[7:])    #[7:len(str)]           #Chakraborty
+
+#Backward Slicing...(Negative Index).
+str = "Apple"
+print(str[-5:-2])                         #App
