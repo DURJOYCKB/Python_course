6dcd00d8c1ba884c5702538bac7b083676ae472f
Durjoy Chakraborty <durjoyckb@gmail.com>
2026-02-04 14:21:14 +0600

    Day 06

---
+++ Day 06/practice1.py
@@ -0,0 +1,12 @@
+marks = {}
+
+x = int(input("enter physics mark : "))
+marks.update({"phy" : x})
+
+x = int(input("enter chemistry mark : "))
+marks.update({"chemistry" : x})
+
+x = int(input("enter biology mark : "))
+marks.update({"biology" : x})
+
+print(marks)
